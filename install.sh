19bd4312fcf58b6eab2a3a4e7cb8bc1024cba879
Paulo Eduardo <pauloes.dev@gmail.com>
2025-07-17 18:57:33 -0300

    Adding omz on the script

--- install.sh
+++ install.sh
@@ -5,11 +5,17 @@ echo "Starging setup..."
 if ! command -v brew &>/dev/null; then
   echo "Homebrew not found. Installing..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo >> /Users/paulo/.zprofile
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/paulo/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 else
   echo "Homebrew is already installed."
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 
 echo "Installing apps from Brewfile..."
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 brew bundle install --file ./Brewfile
 
