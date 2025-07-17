f1e63817a9c9d58fba556106079cd252a046bf38
Paulo Eduardo <pauloes.dev@gmail.com>
2025-07-17 17:10:23 -0300

    fixing install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "Starging setup..."
 
 if ! command -v brew &>/dev/null; then
   echo "Homebrew not found. Installing..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Home/brew/install/HEAD/install.sh)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   echo "Homebrew is already installed."
 fi
